6b152637cde79cb26c2af10eb6e0b116caf64c2a
Eletsia <166839043+Eletsia@users.noreply.github.com>
2025-03-25 19:28:48 +0900

    Update reviewsApi.ts

--- src/api/reviewsApi.ts
+++ src/api/reviewsApi.ts
@@ -26,16 +26,3 @@ export const getReviewByUserId = async (id: string) => {
   return data;
 };
 
-//@param id user의 uid 정보가 포함된 review 데이터
-//@return 추가된 review 데이터 값
-export const addOrderedItems = async (review: review) => {
-  const { data, error, status } = await supabase.from('reviews').insert([review]).select();
-  return data;
-};
-
-//@param 삭제할 리뷰의 review_id 값
-//@return 삭제 결과
-export const deleteOrderItems = async (id: number) => {
-  const { data, error, status } = await supabase.from('reviews').delete().eq('review_id', id);
-  return data;
-};
